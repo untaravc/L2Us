debac60e43db6ee6bafac7817c9b87a26f8548cc
untaravivichahya <untaravivichahya@gmail.com>
2022-10-21 19:46:03 +0700

    create controller

---
+++ app/Http/Controllers/ApiAdmin/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\ApiAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return 'ok';
+    }
+
+    public function logout(){
+        
+    }
+}
